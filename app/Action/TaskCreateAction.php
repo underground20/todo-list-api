2dcfd81722cf6de0e72da5493bb006120d92395b
Nikolay Parshakov <prognik97@gmail.com>
2021-02-10 12:01:59 +0300

    add validation in TaskCreateAction

--- app/Action/TaskCreateAction.php
+++ app/Action/TaskCreateAction.php
@@ -3,6 +3,8 @@
 namespace App\Action;
 
 use App\Service\TaskService;
+use App\Service\ValidatorService;
+use App\Validator\TextLengthConstraint;
 use InvalidArgumentException;
 use Laminas\Diactoros\Response\EmptyResponse;
 use Psr\Http\Message\ResponseInterface;
@@ -12,10 +14,12 @@ use Psr\Http\Server\RequestHandlerInterface;
 class TaskCreateAction implements RequestHandlerInterface
 {
     private TaskService $service;
+    private ValidatorService $validator;
 
-    public function __construct(TaskService $service)
+    public function __construct(TaskService $service, ValidatorService $validator)
     {
         $this->service = $service;
+        $this->validator = $validator;
     }
 
     /**
@@ -25,11 +29,14 @@ class TaskCreateAction implements RequestHandlerInterface
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $param = $request->getParsedBody();
-        if ($text = $param['text']) {
-            $id = $this->service->addTask($text);
+        $param = $request->getParsedBody()['text'];
+        $data = new TextLengthConstraint($param);
+        $errors = $this->validator->validate($data);
+        $message = $this->validator->getErrorMessage($errors);
+        if (!$message) {
+            $this->service->addTask($param);
             return new EmptyResponse(201);
         }
-        throw new InvalidArgumentException('Miss required parameter text', 404);
+        throw new InvalidArgumentException($message, 404);
     }
 }
